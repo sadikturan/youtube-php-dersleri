005c505141d008765383da0cbfe294ae8f93b4a4
sadikturan <sadikturan@gmail.com>
2023-04-02 14:05:22 +0300

    saved

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
+    <title>Document</title>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">CourseApp</a>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Anasayfa</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Kurslar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-3">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action">Programlama</a>
+                    <a href="#" class="list-group-item list-group-item-action">Web Geliştirme</a>
+                    <a href="#" class="list-group-item list-group-item-action">Mobil Uygulama</a>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/1.jpg" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Web Geliştirme</h5>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nostrum dicta dolorem, odit aliquid enim tempore minus totam tenetur animi.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/2.jpg" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Python Kursu</h5>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nostrum dicta dolorem, odit aliquid enim tempore minus totam tenetur animi.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/3.jpg" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Javascript Kursu</h5>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nostrum dicta dolorem, odit aliquid enim tempore minus totam tenetur animi.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
